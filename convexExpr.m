7d825a0522c68b30118c8b55f512a1c41ebb578c
tanmaya11 <113068278+tanmaya11@users.noreply.github.com>
2023-06-21 13:39:08 -0700

    Add files via upload
    convexExpr types fixed

--- convexExpr.m
+++ convexExpr.m
@@ -5,39 +5,55 @@ classdef convexExpr
         psi0=sym('psi0');
         psi1=sym('psi1');
         psi2=sym('psi2');
-        %zeta=sym('zeta');
+        zeta=sym('zeta');
         vpsi0 = functionF.empty();
         vpsi1 = functionF.empty();
         vpsi2 = functionF.empty();
+        vzeta = functionF.empty();
     end
 
     methods
         function obj = convexExpr(type, s0, s1, s2)
             obj.type=type;
-            if nargin == 1
-                obj.vpsi0=functionF(0);  
-              obj.vpsi1=functionF(0);
-              obj.vpsi2=functionF(1);
-            elseif nargin == 2
-              obj.vpsi0=functionF(s0);  
-              obj.vpsi1=functionF(0);
-              obj.vpsi2=functionF(1);
-            elseif nargin == 3
-              obj.vpsi0=functionF(s0);  
-              obj.vpsi1=functionF(s1);
-              obj.vpsi2=functionF(1);
-            else
-              obj.vpsi0=functionF(s0);  
-              obj.vpsi1=functionF(s1);
-              obj.vpsi2=functionF(s2);
-            end
+            %if nargin == 1
+            %    obj.vpsi0=functionF(0);  
+            %  obj.vpsi1=functionF(0);
+            %  obj.vpsi2=functionF(1);
+            %elseif nargin == 2
+            %  obj.vpsi0=functionF(s0);  
+            %  obj.vpsi1=functionF(0);
+            %  obj.vpsi2=functionF(1);
+            %elseif nargin == 3
+            %  obj.vpsi0=functionF(s0);  
+            %  obj.vpsi1=functionF(s1);
+            %  obj.vpsi2=functionF(1);
+            %else
+            %  disp("Error in convexExpr")
+            %end
             d = sym('d');
+            % combine type 2 and 3
             if type == 1
-                obj.expr= obj.psi1^2 /obj.psi2 + obj.psi0;
+                obj.expr = obj.psi1^2 /obj.psi2 + obj.psi0;
+                obj.vpsi0=functionF(s0);  
+                obj.vpsi1=functionF(s1);
+                obj.vpsi2=functionF(s2);
             elseif type ==2
-                obj.expr= -d^2*obj.psi2 + 2* d* obj.psi1 + obj.psi0;
+                %obj.expr= -d^2*obj.psi2 + 2* d* obj.psi1 + obj.psi0;
+                obj.expr = obj.psi1^2 / obj.zeta + obj.psi0;
+                obj.vpsi0=functionF(s0);  
+                obj.vpsi1=functionF(s1);
+                obj.zeta =functionF(s2);
+            
+            elseif type ==3
+                obj.expr= obj.zeta * obj.psi1 + obj.psi0;
+                obj.vpsi0=functionF(s0);  
+                obj.vpsi1=functionF(s1);
+                obj.zeta =functionF(s2);
+            
             else
-                obj.expr=obj.psi1 * obj.psi2 + obj.psi0;
+                disp("Error in convexExpr")
+                %obj.expr= obj.psi1^2 /obj.psi2 + obj.psi0;
+                % psi2 constant quad-quad case
             end
         end
 
